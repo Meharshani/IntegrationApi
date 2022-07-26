817b24c2a4939048487a10de1ad28405aabea862
musman-odoo <muhammadusman_odoo@thinksolutionz.org>
2022-07-27 02:07:01 +0500

    forcast integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { UserdataService } from './services/userdata.service'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
-
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
@@ -12,20 +12,44 @@ export class AppComponent {
   faCoffee = faCoffee;
   title = 'my-dream-app';
   users: any;
+  forcastData: any;
+  Today: any;
   // userd: any;
 
-  WeatherData:any; 
+  WeatherData: any;
 
   ngOnInit() {
     this.WeatherData = {
-      main : {},
-      isDay: !true
-    }; 
+      main: {},
+      isDay: true
+    };
   }
   constructor(private userdata: UserdataService) {
     userdata.user().subscribe((data) => {
+
       console.warn('==>::::>', data);
-      this.users=data
+      this.users = data
+
+    })
+    userdata.forcastData().subscribe((data) => {
+      console.warn('==>::::}}}}>', data);
+
+      let f: any = [data]
+      var forecast: any = f[0];
+      // const [forecastday]:any=forecast;
+      // var arraycontainsturtles = (forecast.indexOf("forecastday"));
+      console.log('==================>,', [forecast][0].forecast.forecastday[0].date_epoch)
+
+      // var someDate = new Date([forecast][0].forecast.forecastday[0].date_epoch);
+      var utcSeconds =new Date(1318023197289);
+       var d = utcSeconds.getDay()
+       const options:any = { weekday: 'long'};
+       console.log(new Intl.DateTimeFormat('en-US', options).format(d));
+       this.Today = new Intl.DateTimeFormat('en-US', options).format(d)
+       let myMoment: moment.Moment = moment(d);
+      console.warn('==>::::}}}}===============>',  moment.utc(d).format("HH:MM"));
+
+      this.forcastData = [forecast][0].forecast.forecastday[0]
 
     })
   }
--- src/app/services/userdata.service.ts
+++ src/app/services/userdata.service.ts
@@ -8,6 +8,10 @@ export class UserdataService {
   constructor(private http:HttpClient) { }
 
   user(){
-    return this.http.get('http://api.weatherapi.com/v1/current.json?key=c6434394d9a84f35ba7194904222507 &q=London&aqi=no')
+    return this.http.get('http://api.weatherapi.com/v1/current.json?key=5bed4cc98d6a40a0bae101754222607 &q=London&aqi=no')
+  }
+  forcastData(){
+    return this.http.get('http://api.weatherapi.com/v1/forecast.json?key=5bed4cc98d6a40a0bae101754222607&q=lahore&days=3&aqi=no&alerts=no')
+    
   }
 }
